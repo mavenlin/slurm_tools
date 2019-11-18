993e2da75fdca7b1859de785a4e3a600dcfeb579
linmin <mavenlin@gmail.com>
2019-11-18 16:19:47 -0500

    fix awk error

--- scripts/slurm_utils.sh
+++ scripts/slurm_utils.sh
@@ -5,9 +5,9 @@ function ssh_nocheck {
 }
 
 function find_port {
-    awk -vLOOKUPVAL=$1 '$1 == LOOKUPVAL { print $2 }' < $(dirname "$0")/slurm_config
+    awk -v LOOKUPVAL=$1 '$1 == LOOKUPVAL { print $2 }' < $(dirname "$0")/slurm_config
 }
 
 function find_all {
-    awk -vLOOKUPVAL=$1 '$1 == LOOKUPVAL { print $2 " " $3 " " $4 }' < $(dirname "$0")/slurm_config
+    awk -v LOOKUPVAL=$1 '$1 == LOOKUPVAL { print $2 " " $3 " " $4 }' < $(dirname "$0")/slurm_config
 }
